f1568cd14d27784ff23afb2587e893ff1bc4f679
SerSurguchev <71214107+SerSurguchev@users.noreply.github.com>
2022-10-21 21:23:06 +0300

    Edit Efficientnet

--- efficientnetv2.py
+++ efficientnetv2.py
@@ -1,23 +1,16 @@
 import torch
 import torch.nn as nn
-import numpy as np
-import yaml
+import math
 
-# SiLU(Swish) activation function
-if hasattr(nn, 'SiLU'):
-    SiLU = nn.SiLU
-else:
-    # for old Pytorch Versions
-    class SiLU(nn.Module):
-        def forward(self, x):
-            return x * torch.sigmoid(x)
-
-def make_divisible(v, divisor=8, min_value=None):
+def _make_divisible(v, divisor, min_value=None):
     """
     This function is taken from the original tf repo.
     It ensures that all layers have a channel number that is divisible by 8
     It can be seen here:
     https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    :param v:
+    :param divisor:
+    :param min_value:
     :return:
     """
     if min_value is None:
@@ -27,6 +20,35 @@ def make_divisible(v, divisor=8, min_value=None):
     if new_v < 0.9 * v:
         new_v += divisor
     return new_v
+
+
+# SiLU (Swish) activation function
+if hasattr(nn, 'SiLU'):
+    SiLU = nn.SiLU
+else:
+    # For compatibility with old PyTorch versions
+    class SiLU(nn.Module):
+        def forward(self, x):
+            return x * torch.sigmoid(x)
+
+ 
+class SELayer(nn.Module):
+    def __init__(self, inp, oup, reduction=4):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+                nn.Linear(oup, _make_divisible(inp // reduction, 8)),
+                SiLU(),
+                nn.Linear(_make_divisible(inp // reduction, 8), oup),
+                nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
+
 
 # First Conv
 class ConvBN_3x3(nn.Sequential):
@@ -51,16 +73,18 @@ class ConvBN_1x1(nn.Sequential):
                     padding=kernel_size//2,
                     bias=False),
             nn.BatchNorm2d(out),
-            SiLU(),                        
+            SiLU(),
         )
 
 # Depthwise Conv
 class Depthwise_conv(nn.Module):
-    def __init__(self, hidden, stride, bias=False):
+    def __init__(self, inp, hidden, stride, 
+                            groups=True, bias=False):
+       
         super(Depthwise_conv, self).__init__()
-        self.conv = nn.Conv2d(hidden, hidden, kernel_size=3,
+        self.conv = nn.Conv2d(inp, hidden, kernel_size=3,
                 stride=stride, padding=1, 
-                groups=hidden, bias=bias)
+                groups=hidden if groups else 1, bias=bias)
 
         self.bn = nn.BatchNorm2d(hidden)
         self.act = SiLU()
@@ -81,56 +105,37 @@ class Pointwise_conv(nn.Module):
     def forward(self, x):
         return self.act(self.bn(self.conv(x) ) )
 
-# SE Layer
-class SELayer(nn.Module):
-    def __init__(self, inp, oup, reduction=4):
-        super(SELayer, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-                nn.Linear(oup, make_divisible(inp // reduction, 8)),
-                SiLU(),
-                nn.Linear(make_divisible(inp // reduction, 8), oup),
-                nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        b, c, _, _ = x.size()
-        y = self.avg_pool(x).view(b, c)
-        y = self.fc(y).view(b, c, 1, 1)
-        return x * y
-
 
 class MBConv(nn.Module):
-    def __init__(self, inp, out, stride, expand_ratio, not_fused):
+    def __init__(self, inp, oup, stride, expand_ratio, not_fuse):
         super(MBConv, self).__init__()
-        assert stride in [1, 2], 'Stride should be equal either 1 or 2'
+        assert stride in [1, 2]
 
-        hidden_dim = int(inp*expand_ratio)
-        self.identity = stride==1 and inp == out
-        
-        if not_fused:
+        hidden_dim = round(inp * expand_ratio)
+        self.identity = stride == 1 and inp == oup
+        if not_fuse:
             self.conv = nn.Sequential(
                 # Depthwise + Pointwise
                 Pointwise_conv(inp, hidden_dim),
-                Depthwise_conv(hidden_dim, stride),
-                # SE layer
+                Depthwise_conv(hidden_dim, hidden_dim, stride),
+                # SE Layer
                 SELayer(inp, hidden_dim),
                 # pw-linear
-                nn.Conv2d(hidden_dim, out, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(out),
-                )
-
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+        # Fused-MBConv
         else:
-                # Fused-MBConv
-                self.conv = nn.Sequential(
+            self.conv = nn.Sequential(
                 # Depthwise 
-                Depthwise_conv(hidden_dim, stride),
+                Depthwise_conv(inp, hidden_dim, stride, groups=False),
                 # SE layer
                 SELayer(inp, hidden_dim),
                 # pw-linear
-                nn.Conv2d(hidden_dim, out, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(out),
-                )
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+
 
     def forward(self, x):
         if self.identity:
@@ -145,41 +150,41 @@ class EffNetV2(nn.Module):
         self.cfgs = cfgs
 
         # building first layer
-        input_channel = make_divisible(24 * width_mult, 8)
-        self.features = [ConvBN_3x3(3, input_channel, 2)]
+        input_channel = _make_divisible(24 * width_mult, 8)
+        layers = [ConvBN_3x3(3, input_channel, 2)]
+
         # building inverted residual blocks
         block = MBConv
-        for t, c, n, s, not_fuse in self.cfgs:
-            output_channel = make_divisible(c * width_mult, 8)
+        for t, c, n, s, use_se in self.cfgs:
+            output_channel = _make_divisible(c * width_mult, 8)
             for i in range(n):
-                self.features.append(block(input_channel, output_channel, s if i == 0 else 1, t, not_fuse))
+                layers.append(block(input_channel, output_channel, s if i == 0 else 1, t, use_se))
                 input_channel = output_channel
 
-        output_channel = make_divisible(1792 * width_mult, 8) if width_mult > 1.0 else 1792
-        print(output_channel)
-        self.features = nn.Sequential(*self.features)
-        print(self.features)
-        self.conv = Pointwise_conv(input_channel, output_channel)
-        # building last several layers
+        output_channel = _make_divisible(1792 * width_mult, 8) if width_mult > 1.0 else 1792
+        layers.append(ConvBN_1x1(input_channel, output_channel))
+
+        self.features = nn.Sequential(*layers)
+
+        # Building last several layers
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.classifier = nn.Linear(output_channel, num_classes)
 
+
     def forward(self, x):
         x = self.features(x)
-        print('Fused Done!')
-        x = self.conv(x)
+#        x = self.conv(x)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
-
 
 def effnetv2_s(**kwargs):
     """
     Constructs a EfficientNetV2-S model
     """
     cfgs = [
-        # t, c, n, s, Fuse
+        # t, c, n, s, SE
         [1,  24,  2, 1, 0],
         [4,  48,  4, 2, 0],
         [4,  64,  4, 2, 0],
@@ -198,5 +203,6 @@ def efficientnetv2_test():
 
 if __name__ == "__main__":
     efficientnetv2_test()
+
 
 
